434cd700dc124820322390c97976fc4b4a654261
Olivia <ramsfol@gmail.com>
2022-10-15 13:40:06 -0500

    added contact form and resume

--- src/components/Project/Resume/index.js
+++ src/components/Project/Resume/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import resume from '../../../assets/photos/resume.png';
 
 function Resume () {
 
     return (
-        <div></div>
+        <div>
+            <a href="https://docs.google.com/document/d/193EX8tUVDqxBDT3spXsQ5Qt9GDhH5NjYQR5pd4mmECQ/edit#">
+                <img src={resume} alt="Olivia Ramsfield's resume."></img>
+            </a>
+        </div>
     )
 }
 
