7527025c2cc84481d1e3d8945f122dc35faa9b6b
Olivia <ramsfol@gmail.com>
2022-10-15 10:03:58 -0500

    added footer with links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaGitlab } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 
 function Footer () {
 
     return (
-        <footer></footer>
+        <footer>
+            <a className="mx-2" href="https://github.com/OliviaRamsfield"><FaGithubSquare /></a>
+            <a className="mx-2" href="https://github.com/OliviaRamsfield"><FaGitlab /></a>
+            <a className="mx-2" href="https://github.com/OliviaRamsfield"><FaLinkedin /></a>
+        </footer>
     )
 }
 
