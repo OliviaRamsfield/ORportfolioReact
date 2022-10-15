d35ea77d6df5c0e33ac404ad656e8a7d151e034e
Olivia <ramsfol@gmail.com>
2022-10-15 09:37:41 -0500

    created header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav/index';
+
+
+function Header() {
+    return (
+        <header className='flex-row'>
+            <h1>Olivia Ramsfield</h1>
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
